ddd04d408f8cd15cea2ed61e237ac533dbde7156
Juan <gurquiza8@gmail.com>
2023-05-18 15:36:54 -0500

    memo, usememo usecallback

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,11 @@ import ReactDOM from 'react-dom/client';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './04-useRef/FocusScreen';
 // import { Layout } from './05-useLayoutEffect/Layout';
-import { Memorize } from './06-memos/Memorize';
+// import { Memorize } from './06-memos/Memorize';
+// import { MemoHook } from './06-memos/MemoHook';
+import { CallbackHook } from './06-memos/CallbackHook';
 import './index.css';
+import { Padre } from './07-tarea-memo/Padre';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -22,7 +25,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <MultipleCustomHooks />
     <FocusScreen />
     <Layout />
-    */}
     <Memorize />
+    <MemoHook />
+    <CallbackHook />
+    */}
+    <Padre />
   </React.StrictMode>,
 )
