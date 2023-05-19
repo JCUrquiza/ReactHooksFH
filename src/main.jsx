a01beb44dd534587296bd05b6e3b152aa5198a01
Juan <gurquiza8@gmail.com>
2023-05-18 19:41:01 -0500

    comienzos con useReducer

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,12 @@ import ReactDOM from 'react-dom/client';
 // import { Layout } from './05-useLayoutEffect/Layout';
 // import { Memorize } from './06-memos/Memorize';
 // import { MemoHook } from './06-memos/MemoHook';
-import { CallbackHook } from './06-memos/CallbackHook';
+// import { CallbackHook } from './06-memos/CallbackHook';
+// import { Padre } from './07-tarea-memo/Padre';
+// import './08-useReducer/intro-reducer';
+
 import './index.css';
-import { Padre } from './07-tarea-memo/Padre';
+import { TodoApp } from './08-useReducer/TodoApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -28,7 +31,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Memorize />
     <MemoHook />
     <CallbackHook />
-    */}
     <Padre />
+    */}
+    <TodoApp />
   </React.StrictMode>,
-)
+ )
